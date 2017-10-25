9b73b6e06f9c3f21e302e3838e0d2a85f5592ba9
Benjamin Shyman <bshyman@gmail.com>
2017-10-24 22:09:11 -0500

    change default port to 5555

--- couchpotato/runner.py
+++ couchpotato/runner.py
@@ -263,7 +263,7 @@ def runCouchPotato(options, base_path, args, data_dir = None, log_dir = None, En
 
     config = {
         'use_reloader': reloader,
-        'port': tryInt(Env.setting('port', default = 5050)),
+        'port': tryInt(Env.setting('port', default = 5555)),
         'host': host if host and len(host) > 0 else '0.0.0.0',
         'host6': host6 if host6 and len(host6) > 0 else '::',
         'ssl_cert': Env.setting('ssl_cert', default = None),
